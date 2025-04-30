ffd9d56780bb778db7e5c8544d2cf4a9e73b81d5
Paribeshlamsal <161189337+Paribeshlamsal@users.noreply.github.com>
2025-04-30 11:40:25 +0545

    Adding success messages

--- lib/firstscreen.dart
+++ lib/firstscreen.dart
@@ -90,43 +90,96 @@ class _FirstScreenState extends State<FirstScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Todo List')),
-      body: ListView.builder(
-        itemCount: todos.length,
-        itemBuilder: (context, index) {
-          final todo = todos[index];
-          return ListTile(
-            leading: IconButton(
-              icon: Icon(
-                todo.isCompleted
-                    ? Icons.check_box
-                    : Icons.check_box_outline_blank,
-                color: todo.isCompleted ? Colors.green : null,
+      body:
+          todos.isEmpty
+              ? const Center(
+                child: Text(
+                  'No any todos',
+                  style: TextStyle(fontSize: 18, color: Colors.grey),
+                ),
+              )
+              : ListView.builder(
+                itemCount: todos.length,
+                itemBuilder: (context, index) {
+                  final todo = todos[index];
+                  return ListTile(
+                    leading: IconButton(
+                      icon: Icon(
+                        todo.isCompleted
+                            ? Icons.check_box
+                            : Icons.check_box_outline_blank,
+                        color: todo.isCompleted ? Colors.green : null,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          todo.isCompleted = !todo.isCompleted;
+                        });
+                      },
+                    ),
+                    title: Text(
+                      todo.title,
+                      style: TextStyle(
+                        decoration:
+                            todo.isCompleted
+                                ? TextDecoration.lineThrough
+                                : null,
+                      ),
+                    ),
+                    subtitle: Text(todo.description ?? ''),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: const Text("Confirm Delete"),
+                              content: const Text(
+                                "Are you sure you want to delete this item?",
+                              ),
+                              actions: [
+                                TextButton(
+                                  child: const Text("Cancel"),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                                TextButton(
+                                  child: const Text("Delete"),
+                                  style: TextButton.styleFrom(
+                                    foregroundColor: Colors.red,
+                                  ),
+                                  onPressed: () {
+                                    setState(() {
+                                      todos.removeAt(index);
+                                    });
+                                    Navigator.of(context).pop();
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(
+                                        content: Text(
+                                          "Item deleted Successfully",
+                                        ),
+                                        duration: Duration(seconds: 2),
+                                        backgroundColor: Color.fromARGB(
+                                          255,
+                                          198,
+                                          62,
+                                          4,
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      },
+                    ),
+                  );
+                },
               ),
-              onPressed: () {
-                setState(() {
-                  todo.isCompleted = !todo.isCompleted;
-                });
-              },
-            ),
-            title: Text(
-              todo.title,
-              style: TextStyle(
-                decoration:
-                    todo.isCompleted ? TextDecoration.lineThrough : null,
-              ),
-            ),
-            subtitle: Text(todo.description ?? ''),
-            trailing: IconButton(
-              icon: const Icon(Icons.delete),
-              onPressed: () {
-                setState(() {
-                  todos.removeAt(index);
-                });
-              },
-            ),
-          );
-        },
-      ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: _addTodo,
         child: const Icon(Icons.add),
